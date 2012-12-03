e9b6d9a4bc41ac380cef55c49d28adeb9fcc2d5d
Beoran <beoran@rubyforge.org>
2012-12-03 23:48:10 +0100

    blank Allegro application

--- src/main.c
+++ src/main.c
@@ -1,9 +1,50 @@
 
 #include "alps.h"
+#include <stdlib.h>
+
+#define SCREEN_W 640
+#define SCREEN_H 480
+
+
+int main_loop(ALLEGRO_DISPLAY * display, ALLEGRO_EVENT_QUEUE   * queue) {
+  int busy = 1;
+  ALLEGRO_EVENT event;
+  while (busy) {
+    while(al_get_next_event(queue, &event)) {
+      if(event.type == ALLEGRO_EVENT_KEY_DOWN) { 
+        switch(((ALLEGRO_KEYBOARD_EVENT*)&event)->keycode) {
+          case ALLEGRO_KEY_ESCAPE:
+             busy = 0;
+          default:
+            break;
+        }
+      }
+    }
+    /* Draw here: */
+    
+    
+  }
+  return busy;
+}
+
 
 
 int main(void) {
-	
-
-	return 0;
+  ALLEGRO_DISPLAY       * display;
+  ALLEGRO_EVENT_QUEUE   * queue;
+  al_init();
+  display = al_create_display(SCREEN_W, SCREEN_H);
+  if(!display)  return 1;
+  queue   = al_create_event_queue();
+  if(!queue)    return 2;
+  
+  al_install_keyboard();
+  al_install_mouse();
+  al_register_event_source(queue, al_get_keyboard_event_source());
+  al_register_event_source(queue, al_get_mouse_event_source());
+  
+  main_loop(display, queue);
+  
+  al_destroy_display(display);
+  return 0;
 }
